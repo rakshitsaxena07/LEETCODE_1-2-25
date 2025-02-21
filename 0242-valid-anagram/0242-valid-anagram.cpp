b285b96bd10166bc26bbfe9f9ce00fc559e6105b
Rakshit saxena <115712754+rakshitsaxena07@users.noreply.github.com>
2025-02-21 11:52:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<int,int>ump;
+
+        for(int i=0;i<t.length();i++){
+            ump[t[i]]++;
+        }
+        for(int i=0;i<t.length();i++){
+            if(ump.find(s[i])!=ump.end()){
+                if(ump[s[i]]>0)
+                ump[s[i]]--;
+
+                else{
+                    return 0;
+                }
+            }
+            else{return 0;}
+        }
+        return 1;
+    }
+};
