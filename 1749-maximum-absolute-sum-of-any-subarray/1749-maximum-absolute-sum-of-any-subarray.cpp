9d996164449cf074b3b970a731d3b78ad30c0513
Rakshit saxena <115712754+rakshitsaxena07@users.noreply.github.com>
2025-02-26 14:31:39 +0530

    Time: 2 ms (27.8%), Space: 48.5 MB (6.06%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -1,44 +1,44 @@
 class Solution {
 public:
-    int minKadaneAlgo(vector<int>nums,int n){
-         int maxi=INT_MIN;
-     int sum=0;
+        int minKadaneAlgo(vector<int>nums,int n){
+            int maxi=INT_MIN;
+        int sum=0;
 
-     for(int i=0;i<n;i++){
-        sum+=nums[i];
-        maxi=max(maxi,sum);
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            maxi=max(maxi,sum);
 
-        if(sum<0){
-            sum=0;
+            if(sum<0){
+                sum=0;
+            }
+
+        
         }
-
-    
-     }
-        return maxi;
-    }
-    int maxKadaneAlgo(vector<int>nums,int n){
-         int mini=INT_MAX;
-     int sum=0;
-
-     for(int i=0;i<n;i++){
-        sum+=nums[i];
-        mini=min(mini,sum);
-
-        if(sum>0){
-            sum=0;
+            return maxi;
         }
+        int maxKadaneAlgo(vector<int>nums,int n){
+            int mini=INT_MAX;
+        int sum=0;
 
-    
-     }
-        return mini;
-    }
-    int maxAbsoluteSum(vector<int>& nums) {
-        int n=nums.size();
-        int a=maxKadaneAlgo(nums,n);
-        int b=minKadaneAlgo(nums,n);
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            mini=min(mini,sum);
 
-        return max(abs(a),abs(b));
-    }
+            if(sum>0){
+                sum=0;
+            }
+
+        
+        }
+            return mini;
+        }
+        int maxAbsoluteSum(vector<int>& nums) {
+            int n=nums.size();
+            int a=maxKadaneAlgo(nums,n);
+            int b=minKadaneAlgo(nums,n);
+
+            return max(abs(a),abs(b));
+        }
 
     
 };
