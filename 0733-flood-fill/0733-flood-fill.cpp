b883cd13c9a5cc2d99061f94fa4a9bf2eebe8f25
Rakshit Saxena <115712754+rakshitsaxena07@users.noreply.github.com>
2025-06-09 12:14:58 +0530

    Time: 4 ms (6.71%), Space: 16.5 MB (100%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+      void dfstra(vector<vector<int>> &image, int row, int column, int inicol, int newcol, int deltarow[], int deltacol[]) {
+    image[row][column] = newcol;
+    int n = image.size(); // for row size
+    int m = image[0].size(); // for column size:it calculates no.of elements in 0th row
+    // since every time 4 adj will be found
+    for (int i = 0; i < 4; i++) {
+        int nrow = row + deltarow[i];
+        int ncol = column + deltacol[i];
+        if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && image[nrow][ncol] == inicol && image[nrow][ncol] != newcol) {
+            dfstra(image, nrow, ncol, inicol, newcol, deltarow, deltacol);//recursively traversing DFS
+        }
+    }
+}
+
+public:
+    vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int color) {
+        int inicol = image[sr][sc];
+        //vector<vector<int>> res = image;
+        int deltarow[] = {-1,1,0,0};
+        int deltacol[] = {0,0,-1,1};
+        dfstra(image, sr, sc, inicol, color, deltarow, deltacol);
+        return image;
+    }
+
+};
