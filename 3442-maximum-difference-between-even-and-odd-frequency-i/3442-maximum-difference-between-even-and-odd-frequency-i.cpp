1e22f5623882a89a03b952a02d5dd127451ac2b5
Rakshit Saxena <115712754+rakshitsaxena07@users.noreply.github.com>
2025-06-10 21:55:39 +0530

    Time: 0 ms (100%), Space: 10.1 MB (16.7%) - LeetHub

---
+++ 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxDifference(string s) {
+        unordered_map<char,int>ump;
+        for(auto it:s){
+            ump[it]++;
+        }
+        vector<int>res;
+
+        for(auto it:ump){
+            res.push_back(it.second);
+        }
+        
+        int maxi=0,mini=102;
+        for(auto it:ump){
+            if(it.second %2!=0){
+                maxi=max(maxi,it.second);
+            }
+            else{
+                mini=min(mini,it.second);
+            }
+        }
+
+        return maxi-mini;
+    }
+};
