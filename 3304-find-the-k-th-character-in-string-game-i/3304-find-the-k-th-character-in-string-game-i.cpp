8f3f5a5e43ecd210c0c1446786364573ddc72707
Rakshit Saxena <115712754+rakshitsaxena07@users.noreply.github.com>
2025-07-03 11:32:37 +0530

    Time: 0 ms (100%), Space: 8.6 MB (82.54%) - LeetHub

--- 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.cpp
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.cpp
@@ -4,7 +4,7 @@ public:
     vector<char> res;
     res.push_back('a');
 
-    while (res.size() <= k) {
+    while (res.size() < k) {
         int sz = res.size();  // capture current size before appending
         for (int j = 0; j < sz; ++j) {
             char nextChar = (res[j] - 'a' + 1) % 26 + 'a';
