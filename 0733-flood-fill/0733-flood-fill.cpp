9be9e4df3e3baa4d1a38785b8cb418c30c9fa71f
Rakshit Saxena <115712754+rakshitsaxena07@users.noreply.github.com>
2025-07-03 12:09:35 +0530

    Time: 0 ms (100%), Space: 17.9 MB (90.52%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,7 +1,8 @@
 class Solution {
 private:
       void dfstra(vector<vector<int>> &image, int row, int column, int inicol, int newcol, int deltarow[], int deltacol[]) {
-    image[row][column] = newcol;
+    image[row][column] = newcol;//assigning the new color
+    
     int n = image.size(); // for row size
     int m = image[0].size(); // for column size:it calculates no.of elements in 0th row
     // since every time 4 adj will be found
