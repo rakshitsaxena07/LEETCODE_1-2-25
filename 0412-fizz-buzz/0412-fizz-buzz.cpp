ebd6db33b7a3ee4096ae3449112e35c133603575
Rakshit Saxena <115712754+rakshitsaxena07@users.noreply.github.com>
2025-07-15 00:47:11 +0530

    Time: 0 ms (100%), Space: 11.7 MB (44.47%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        if(n==1){
+            return {to_string(1)};
+        }
+        vector<string> res;
+        for(int i=1;i<=n;i++){
+            if(i%3==0 and i%5==0){
+                res.push_back("FizzBuzz");
+            }
+            else if(i%3==0){
+                res.push_back("Fizz");
+            }
+            else if(i%5==0){
+                 res.push_back("Buzz");
+            }
+            else{
+                res.push_back(to_string(i));
+            }
+        }
+
+        return res;
+    }
+};
