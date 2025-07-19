5401f8d9cee9c8dce3e1cd80018b14274c924505
Rakshit Saxena <115712754+rakshitsaxena07@users.noreply.github.com>
2025-07-19 18:22:53 +0530

    Time: 47 ms (84.09%), Space: 48.6 MB (76.99%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.cpp
@@ -5,7 +5,7 @@ public:
     vector<string> result;
     result.push_back(folder[0]);
     for (int i = 1; i < folder.size(); ++i) {
-        string& last = result.back();
+        string last = result.back();
         string& current = folder[i];
         if (current.find(last + "/") != 0) {
             result.push_back(current);
