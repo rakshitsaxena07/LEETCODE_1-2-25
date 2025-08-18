f494d851c027df8a287ff947a45e779660a370d5
Rakshit Saxena <115712754+rakshitsaxena07@users.noreply.github.com>
2025-08-18 18:58:19 +0530

    Time: 0 ms (100%), Space: 65.1 MB (42.51%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-       int n=nums.size();
-        vector<int>ve(n,0);//initially all 0
-        for(int i=0;i<n;i++){
-            int t=nums[i];
-            if(ve[t]==0){
-                ve[t]=1;//to mark visited
-            }
-            else{
-                return t;
-            }
-        }
-        return 0;
+        int slow = nums[0];
+    int fast = nums[0];
+    
+    // Phase 1: Find the intersection point
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow != fast);
+    
+    // Phase 2: Find the entrance to the cycle
+    slow = nums[0];
+    while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[fast];
     }
+    
+    return slow;
+}
 };
