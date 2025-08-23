1c2683060a62df92a3779cc5197afd04672285fa
Rakshit Saxena <115712754+rakshitsaxena07@users.noreply.github.com>
2025-08-23 22:03:58 +0530

    Time: 3 ms (89.51%), Space: 23.9 MB (57.18%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,25 +4,14 @@ public:
             if (intervals.empty()) {
         return {};
     }
-    
-    // Sort intervals based on the start value
-    sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
-        return a[0] < b[0];
-    });
-    
-    vector<vector<int>> merged;
-    merged.push_back(intervals[0]);
-
- for (size_t i = 1; i < intervals.size(); ++i) {
-        vector<int>& last = merged.back();
-        if (intervals[i][0] <= last[1]) {
-            // Overlapping intervals, merge them
-            last[1] = max(last[1], intervals[i][1]);
-        } else {
-            merged.push_back(intervals[i]);
+    sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+     
+        ans.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]>ans.back()[1]) ans.push_back(intervals[i]);
+            else ans.back()[1]=max(ans.back()[1],intervals[i][1]);
         }
+        return ans;
     }
-    
-    return merged;
-}
 };
