743809064b4bd772300ec0810565274e7d466c85
Rakshit Saxena <115712754+rakshitsaxena07@users.noreply.github.com>
2025-08-29 21:24:26 +0530

    Time: 0 ms (100%), Space: 21.6 MB (93.72%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        if(intervals.empty()){
-            return {newInterval};
-        }
-        int start=newInterval[0];
-        int end=newInterval[1];
-        
-
-        for(int i=0;i<intervals.size();i++){
-           if(intervals[i][1]>=start){
-            intervals[i][1]=max(intervals[i][1], end);
-            break;
-           }
-        }
-
-
-        vector<vector<int>> res;
-        for(int i=0;i<intervals.size();i++){
-                if(res.empty() or res.back()[1]<intervals[i][0]){
-                    res.push_back(intervals[i]);
-                }
-                else{
-                    res.back()[1]=max(res.back()[1],intervals[i][1]);
-                }
-        }
-
-        return res;
+        vector<vector<int>> result;
+    int i = 0;
+    int n = intervals.size();
+    
+    // Add all intervals that end before newInterval starts
+    while (i < n && intervals[i][1] < newInterval[0]) {
+        result.push_back(intervals[i]);
+        i++;
     }
+    
+    // Merge all overlapping intervals
+    while (i < n && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = min(newInterval[0], intervals[i][0]);
+        newInterval[1] = max(newInterval[1], intervals[i][1]);
+        i++;
+    }
+    result.push_back(newInterval);
+    
+    // Add all remaining intervals
+    while (i < n) {
+        result.push_back(intervals[i]);
+        i++;
+    }
+    
+    return result;
+}
 };
