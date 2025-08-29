8ac9c1e220f3161ce220563ec0b5b1ea1a9a3817
Rakshit Saxena <115712754+rakshitsaxena07@users.noreply.github.com>
2025-08-29 14:09:36 +0530

    Time: 0 ms (100%), Space: 8.5 MB (12.44%) - LeetHub

---
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    long long flowerGame(int n, int m) {
+          long long n_even = n / 2;
+    long long n_odd = (n + 1) / 2;
+    long long m_even = m / 2;
+    long long m_odd = (m + 1) / 2;
+    return n_even * m_odd + n_odd * m_even;
+    }
+};
